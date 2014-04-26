5c7d93c84c70abbc8c5bdb27a057dde60e79d9ae
Casey Robinson <kc@rampantmonkey.com>
2014-04-26 17:44:17 -0400

    create method to reset cursor to first element inserted

--- test/doublycircular-test.js
+++ test/doublycircular-test.js
@@ -19,4 +19,19 @@ describe('DoublyCircular', function () {
       assert.equal(list.current, '4')
     })
   })
+
+  describe('#reset()', function () {
+    before(function () {
+      list = new DoublyCircular()
+      for (var i = 0; i < 15; i++) {
+        list.push(15-i)
+      }
+    })
+
+    it('moves current to first element inserted', function () {
+      assert.equal(list.current, '1')
+      list.reset()
+      assert.equal(list.current, '15')
+    })
+  })
 })
