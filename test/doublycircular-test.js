578e8b56db3fbad0f61aba61d764b3b38c5840ee
Casey Robinson <kc@rampantmonkey.com>
2014-04-26 18:22:29 -0400

    Iterate in the opposite direction
    The % + % silliness in the tests is to replicate the expected modulo
    behavior for negative numbers (where 'expected' comes from Abstract
    Algebra).

--- test/doublycircular-test.js
+++ test/doublycircular-test.js
@@ -65,4 +65,35 @@ describe('DoublyCircular', function () {
       }
     })
   })
+
+  describe('#prev()', function () {
+    before(function () {
+      list = new DoublyCircular()
+      for (var i = 0; i < 10; i++) {
+        list.push(i)
+      }
+    })
+
+    beforeEach(function () {
+      list.reset()
+    })
+
+    it('returns the current item in the list and moves the current item pointer backwards', function () {
+      assert.equal(list.current, 0)
+      assert.equal(list.prev(), 0)
+      assert.equal(list.current, 9)
+    })
+
+    it('goes the whole way around once', function () {
+      for (var i = 0; i < 10; i++) {
+        assert.equal(list.prev(), ((-i)%10+10)%10)
+      }
+    })
+
+    it("goes 'round-and-'round", function () {
+      for (var i = 0; i < 111; i++) {
+        assert.equal(list.prev(), ((-i)%10+10)%10)
+      }
+    })
+  })
 })
